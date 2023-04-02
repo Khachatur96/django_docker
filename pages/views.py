6b2a984d4b6562cefaeb4e6f68590709c3aaca94
kgachatur96 <www.xachatur@mail.ru>
2023-04-02 18:30:59 +0400

    ch6

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,9 @@ from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 
 
-
 class HomePageView(TemplateView):
     template_name = 'home.html'
-    
 
+
+class AboutPageView(TemplateView):  # new
+    template_name = 'about.html'
